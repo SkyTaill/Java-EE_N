1aebd8b25964f370dc92b3dfe79742b8af138c6c
maximVorobeichik <vorobeichik2015@yandex.ru>
2021-02-10 20:00:51 +0300

    add new DZ

---
+++ JavaEE-01-/First-Web-App/src/main/java/ru/geekbrains/HttpServlet/Cart.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.HttpServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/http-cart"})
+public class Cart extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("pageHeader","Server elem");
+        getServletContext().getRequestDispatcher("/page_header").include(req,resp);
+        resp.getWriter().println("<h1>Cart</h1>");
+    }
+}
